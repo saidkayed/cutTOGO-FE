b286b2acdff3e551d489496505aff36c55c6f642
Your Name <you@example.com>
2020-02-13 12:01:01 +0100

    login modal createt

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -1,15 +1,54 @@
 import React from 'react'
 import './Modal.css'
+import './Login.css'
+
+
+
+
+
+
 
 const Modal = (props) => {
+    
+    
+    const login = () =>{
+       return( 
+       <div id="myModal" class="modal">
+        <div class="modal-content">
+            <a onClick={props.loginModalClick}><span class="close">&times;</span></a>
+            <p><b>cutTOGO</b></p>
+            
+            <label for="uname"><b>Email</b></label>
+            
+            <input type="text" placeholder="Enter Username" name="uname" required></input>
+            
+            <label for="psw"><b>Adgangskode</b></label>
+            
+            <input type="password" placeholder="Enter Password" name="psw" required></input>
+            
+            <button id="loginBTN" type="submit">Login</button>
+        </div>
+    </div>
+       );
+    }
+
+    const create = () =>{
+
+
+
+        
+    }
+    
+    
+    
+    
+    
+    
     return (
         <div>
-            <div id="myModal" class="modal">
-                <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <p>Some text in the Modal..</p>
-                </div>
-            </div>
+            {props.loginModal === true ?
+            login()
+                : null}
         </div>
     );
 }
